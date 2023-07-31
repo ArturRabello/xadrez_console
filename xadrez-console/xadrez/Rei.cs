9eb193ead61e6b22634d63dae3ecde8f8dac4ed5
Artur Rabello <artur_rabello@yahoo.com.br>
2023-07-31 15:31:31 -0300

    adicionando pecas no tabuleiro

---
+++ xadrez-console/xadrez/Rei.cs
@@ -0,0 +1,18 @@
+﻿using tabuleiro;
+
+namespace xadrez
+{
+    class Rei : Peca
+    {
+        public Rei(Tabuleiro Tab, Cor cor) : base(Tab, cor)
+        {
+
+        }
+
+        public override string ToString()
+        {
+            return "R";
+        }
+
+    }
+}
---
+++ xadrez-console/xadrez/Torre.cs
@@ -0,0 +1,18 @@
+﻿using tabuleiro;
+
+namespace xadrez
+{
+    class Torre : Peca
+    {
+        public Torre(Tabuleiro Tab, Cor cor) : base(Tab, cor)
+        {
+
+        }
+
+        public override string ToString()
+        {
+            return "T";
+        }
+
+    }
+}
