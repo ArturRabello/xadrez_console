261528417a5a4db6a2e2119567197a75cb7fb553
Artur Rabello <artur_rabello@yahoo.com.br>
2023-08-07 16:42:39 -0300

    Acrescentando as demais pecas

--- xadrez-console/Tela.cs
+++ xadrez-console/Tela.cs
@@ -15,11 +15,22 @@ namespace xadrez_console
             Console.WriteLine();
             imprimirPecasCapturadas(partida);
             Console.WriteLine("Turno: " + partida.turno);
-            Console.WriteLine("Agurdando jogada: " + partida.jogadorAtual);
-            if (partida.xeque)
+            if (!partida.terminada)
             {
-                Console.WriteLine("XEQUE!");
+                Console.WriteLine("Agurdando jogada: " + partida.jogadorAtual);
+                if (partida.xeque)
+                {
+                    Console.WriteLine("XEQUE!");
+                }
             }
+            else
+            {
+                Console.WriteLine("XEQUEMATE!");
+                Console.WriteLine("Vencedor: " + partida.jogadorAtual);
+            }
+
+
+            
         }
 
         public static void  imprimirPecasCapturadas(PartidaDeXadrez partida)
